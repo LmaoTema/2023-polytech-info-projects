0653253dfc7f7ba6829743b251bc00582f82b677
LmaoTema <tikhonravoff2016@yandex.ru>
2023-02-20 19:15:56 +0300

    add sin,cos,tg operators

--- 02_Calc/logic.cpp
+++ 02_Calc/logic.cpp
@@ -1,5 +1,6 @@
 #include "logic.h"
 #include <sstream>
+#include <cmath>
 
 namespace calc
 {
@@ -13,12 +14,12 @@ namespace calc
         std::istringstream input_ss(expression);
         std::stringstream output_ss;
 
-        int a;
-        int b;
+        double a;
+        double b;
         char op;
         input_ss >> a >> op >> b;
 
-        if (op == '+')
+         if (op == '+')
         {
             output_ss << a << op << b << "=" << a + b;
         }
@@ -43,6 +44,24 @@ namespace calc
                 {
                     output_ss << a / b;
                 }
+            }
+            else if (op == 's') // добавляем поддержку функции синуса
+            {
+                output_ss << "sin(" << a << ")=" << sin(a * M_PI / 180.0);
+            }
+            else if (op == 'c') // добавляем поддержку функции косинуса
+            {
+                output_ss << "cos(" << a << ")=" << cos(a * M_PI / 180.0);
+            }
+            else if (op == 't') // добавляем поддержку функции тангенса
+            {
+                if (cos(a * M_PI / 180.0) == 0) // Проверяем, что знаменатель не равен нулю
+            {
+                output_ss << "Error: tan(" << a << ") is undefined";
+            }
+            else
+            {
+                output_ss << "tan(" << a << ")=" << tan(a * M_PI / 180.0)
             }
         }
         return output_ss.str();
